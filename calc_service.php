4c5928cce009953436b10fbf7dbe396195e896f9
darakraft <55558964+darakraft@users.noreply.github.com>
2024-10-26 14:13:45 +0300

    Add files via upload

---
+++ calc_service.php
@@ -0,0 +1,34 @@
+<?php 
+        session_start();
+      
+        $x = $_REQUEST["txtX"];
+        $y = $_REQUEST["txtY"];
+        if (isset($_REQUEST["plus"]) && $_REQUEST["plus"] == "true") {
+            $z = $x + $y;
+            $operation = "plus";
+        }
+        else {
+            $z = $x - $y;
+            $operation = "minus";
+        }
+
+        $user = $_SESSION["user"];
+
+        //так писать не стоит: уязвимость SQL Injection
+        $sql = "
+        INSERT INTO calcs(UserName, Operation, Number1, Number2, Result)
+        VALUE('$user','$operation', $x, $y, $z)
+        ";
+        
+        // Нарушение приципа наименьших привилегий (root)
+        // Слабый пароль
+        // Секрет в коде
+        $conn = mysqli_connect("localhost", "root", "", "calc");
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+
+        sleep(1);
+
+        echo($z);
+
+  ?>
