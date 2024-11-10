ee7690ccde80430e49fe5284ae991df7e13ec388
darakraft <55558964+darakraft@users.noreply.github.com>
2024-11-10 11:13:36 +0300

    Add files via upload

--- calc_modern.php
+++ calc_modern.php
@@ -1,102 +1,123 @@
 <?php
     session_start();
     if (!isset($_SESSION["user"])) {
-    echo('<meta http-equiv="refresh" content="3; URL=login.html">');
-        die("You need login to open this page! Вы будете перенеправлены");
-  }
+		echo("<meta http-equiv='refresh' content='1; URL=login.html'>");
+        die("Требуется логин ! Вы будете перенаправлены на страницу авторизации.");
+	}
 ?>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Calc</title>
-    <style>
-      input {
-        /* it is css comment */
-        width: 160px;
-        margin: 2px;
-        text-align: center;
-      }
+	<head>
+		<meta charset="UTF-8">
+		<title>Калькулятор</title>
+		<style>
+			input {
+				/* it is css comment */
+				width: 160px;
+				margin: 2px;
+				text-align: center;
+			}
 
-      button {
-        width: 70px;
-        background-color: darkgrey;
-      }
+			button {
+				border: solid black thin;
+				border-radius: 3%;
+				width: 75px;
+				margin: 2px;
+				background: #dbdada;
+				color: black;
+			}
 
-      .calculator {
-        border: solid black thin;
-        width: 180px;
-        padding: 10px;
-        text-align: center;
-        background-color: burlywood;
-      }
+			body {
+				text-align: center;
+				align-items: center;
+				margin-left: auto;
+				margin-right: auto;
+			}
 
-      .pressed {
-        background-color: #FFFF55;
-      }
+			.calculator {
+				border: solid black thin;
+				border-radius: 3%;
+				width: 210px;
+				padding: 10px;
+				text-align: center;
+				align-items: center;
+				background-color: rgb(211, 255, 255);
+				margin-left: auto;
+				margin-right: auto;
+			}
 
-      img {
-        height: 200px;
-      }
-    </style>
+			.pressed {
+				background-color: #FFFF55;
+			}
 
-    <script>
-      function plus() {
-        var url = "api/calc_service.php?txtX=" 
-              + document.getElementById("txtX").value 
-              + "&txtY= "
-              + document.getElementById("txtY").value
-              + "&plus=true";
+			img {
+				height: 200px;
+			}
+		</style>
+	<script>
+		function plus() {
+			var url = "api/calc_service.php?plus" + "&txtX="
+						+ document.getElementById("txtX").value 
+						+ "&txtY="
+						+ document.getElementById("txtY").value;
 
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-        xhr.onload = function() {
-          var result = xhr.responseText;
-          //alert(result);
-          document.getElementById("txtZ").value = result;
-        }
-        document.getElementById("txtZ").value = "WAIT...";
-        xhr.send();
-        
-      }
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", url);
+			xhr.onload = function() {
+				var result = xhr.responseText;
+				document.getElementById("txtZ").value = result;
+			}
+			document.getElementById("txtZ").value = "Подождите...";
+			xhr.send();
+			
+		}
 
-      function minus() {
-                var url = "api/calc_service.php?txtX=" 
-                          + document.getElementById("txtX").value 
-                          + "&txtY="
-                          + document.getElementById("txtY").value
-                          + "&plus=false";
+		function minus() {
+			var url = "api/calc_service.php?minus" + "&txtX="
+						+ document.getElementById("txtX").value 
+						+ "&txtY= "
+						+ document.getElementById("txtY").value;
 
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", url);
-                xhr.onload = function() {
-                    var result = xhr.responseText;
-                    //alert(result);
-                    document.getElementById("txtZ").value = result;
-                }
-                document.getElementById("txtZ").value = "WAIT...";
-                xhr.send();  
-            }
-    </script>
-  
-  </head>
-  <body>
-    <!-- It is comment -->
-    <h1>Калькулятор with AJAX</h1>
-    <div>Our unique calculator. Try it!</div>
-    
-    <div class="calculator">
-        <input id="txtX" autocomplete="off" /> <br />
-        <input id="txtY" autocomplete="off"/> <br />
-        <div class="buttons">
-          <button id="plus" onclick="plus();">+</button>
-          <button id="minus">-</button>
-        </div>
-        <input id="txtZ" />
-    </div>
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", url);
+			xhr.onload = function() {
+				var result = xhr.responseText;
+				document.getElementById("txtZ").value = result;
+			}
+			document.getElementById("txtZ").value = "Подождите...";
+			xhr.send();
+			
+		}
 
-    <textarea>
-      
-    </textarea>
-  </body>
-  
+		function multiply() {
+			var url = "api/calc_service.php?multiply" + "&txtX="
+						+ document.getElementById("txtX").value 
+						+ "&txtY= "
+						+ document.getElementById("txtY").value;
+
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", url);
+			xhr.onload = function() {
+				var result = xhr.responseText;
+				document.getElementById("txtZ").value = result;
+			}
+			document.getElementById("txtZ").value = "Подождите...";
+			xhr.send();
+			
+		}
+
+	</script>
+	</head>
+		<body>
+			<h1>Калькулятор на AJAX</h1>
+			<div class="calculator">
+					<input id="txtX" autocomplete="off"/> <br />
+					<input id="txtY" autocomplete="off"/> <br />
+					<div class="buttons">
+						<button id="plus" onclick="plus()">+</button>
+						<button id="minus" onclick="minus()">-</button>
+						<button id="multiply" onclick="multiply()">x</button>
+					</div>
+					<input id="txtZ"/>
+			</div>
+		</body>
 </html>
